07f20525b02344a2ee4311a80b3ac7ef03e3d666
Joao-Honorato <167466646+Joao-Honorato@users.noreply.github.com>
2024-06-01 12:58:01 -0300

    Update bd.php

--- include/bd.php
+++ include/bd.php
@@ -1,18 +1,1 @@
-<?php
-$dbHost = "LocalHost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "LavaRapido";
 
-$conn = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
-
-    if ($conn -> connect_errno){
-        echo "ERRO AO CONECTAR";
-
-
-    }
-    else {
-        echo "conexão efetuada com sucesso!";
-    }
-    
-?>
